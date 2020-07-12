26f5790fa2cb1c58e60568cb8fcbdec2f035e119
Jonathan Martinez <jonathan@MacBook-Pro-de-Jonathan.local>
2020-07-12 12:24:18 +0200

    banner

--- shortly_code/src/components/Banner.js
+++ shortly_code/src/components/Banner.js
@@ -1,12 +1,35 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-const BannerStyled = styled.div``
+const BannerStyled = styled.section`
+	background-color: var(--darkViolet);
+	background-image: url('images/bg-boost-mobile.svg');
+	text-align: center;
+	padding: 5.625em 1.6em;
+	margin-top: 4.563em;
+	h2 {
+		font-size: 1.75rem;
+		margin-bottom: 1em;
+		color: white;
+	}
+	a {
+		border: 2px solid transparent;
+		border-radius: 0.313em;
+		padding: 0.6em 1.8em;
+		font-size: 1.125rem;
+		color: white;
+		background-color: var(--cyan);
+		font-weight: 700;
+		text-decoration: none;
+		border-radius: 25px;
+	}
+`
 
 function Banner() {
 	return (
 		<BannerStyled>
-			<div>Banner</div>
+			<h2>Boost your links today</h2>
+			<a href="https://google.es">Get Started</a>
 		</BannerStyled>
 	)
 }
